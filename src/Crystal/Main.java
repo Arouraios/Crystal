974c7fb3d39b383c5e9e94376efd56b27bfae160
Arouraios <lao_shu@gmx.de>
2018-02-04 15:18:17 +0100

    changes

--- src/Crystal/Main.java
+++ src/Crystal/Main.java
@@ -1,5 +1,20 @@
 package Crystal;
 
-public class Main {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin{
+	public void onEnable(){
+		System.out.println("Vorlage gestartet");
+	}
+	public void onDisable(){
+		System.out.println("Vorlage gestoppt");
+	}
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("Vorlage")){
+			return true;
+		}
+		return false;
+	}
 }
