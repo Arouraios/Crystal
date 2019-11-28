28f6548431ba583f40bd5598cb9e3791a78fde85
Arouraios <lao_shu@gmx.de>
2019-11-28 14:23:20 +0100

    cleanup

---
+++ workspaceold/Crystal/src/me/Arouraios/Crystal/objects/Crystal.java
@@ -0,0 +1,13 @@
+package me.Arouraios.Crystal.objects;
+
+import me.Arouraios.Crystal.minigame.Game;
+import me.Arouraios.Crystal.teams.Team;
+
+public class Crystal {
+	private int health;
+	private String color;
+	private Team t;
+	private Game g;
+	
+	public Crystal(int health, )
+}
